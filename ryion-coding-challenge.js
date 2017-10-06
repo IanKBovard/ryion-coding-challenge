defbb017c3933591294a9f79951b59f044b30ade
Ian Bovard <thebestpantsintown@gmail.com>
2017-10-06 12:22:42 -1000

    diagram hardcoded, next dynamically add data

--- ryion-coding-challenge.js
+++ ryion-coding-challenge.js
@@ -1,83 +1,15 @@
 /* jshint esversion:6*/
-const PIPE_SIZES = [
-  8,
-  10,
-  12,
-  16,
-  20,
-  25,
-  32,
-  40,
-  50,
-  63,
-  80,
-  100,
-  127,
-  160,
-  201,
-  254,
-  320,
-  404,
-  509,
-  642,
-  810,
-  1021,
-  1288,
-  1624,
-  2048,
-  2582,
-  3256,
-  4106,
-  5178,
-  6529,
-  8233,
-  10382,
-  13091,
-  16507,
-  20815,
-  26248,
-  33098,
-  41735,
-  52627,
-  66361,
-  83680,
-  105518,
-  133056,
-  167780,
-  211566,
-  250000,
-  300000,
-  350000,
-  400000,
-  450000,
-  500000,
-  600000,
-  700000,
-  750000,
-  800000,
-  900000,
-  1000000,
-  1250000,
-  1500000,
-  1750000,
-  2000000
-];
-
+const PIPE_SIZES = [8, 10, 12, 16, 20, 25, 32, 40, 50, 63, 80, 100, 127, 160, 201, 254, 320, 404, 509, 642, 810, 1021, 1288, 1624, 2048, 2582, 3256, 4106, 5178, 6529, 8233, 10382, 13091, 16507, 20815, 26248, 33098, 41735, 52627, 66361, 83680, 105518, 133056, 167780, 211566, 250000, 300000, 350000, 400000, 450000, 500000, 600000, 700000, 750000, 800000, 900000, 1000000, 1250000, 1500000, 1750000, 2000000];
 const VALVE_SIZES = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100];
 const TOLERANCE = 0.02;
 
-let pipeSizeX;
-let valveSizeX;
-let pipeSizeY;
-let valveSizeY;
-let rateZ;
-let pipeSizeZ;
+let pipeX = { pipeSize: null, valveSize: null };
+let pipeY = { pipeSize: null, valveSize: null };
+let pipeZ = { rate: null, pipeSize: null };
 
-let minPipeSizeX;
-let minValveSizex;
-let minPipeSizeY;
-let minValveSizeY;
-let minPipeSizeZ;
+let minPipeX = { pipeSize: null, valveSize: null };
+let minPipeY = { pipeSize: null, valveSize: null };
+let minPipeZ = { pipeSize: null };
 
 function calculatePipeSize(rate) {
   let pipeSize = 1.732 * 12.9 * 200 * rate / (208 * TOLERANCE);
@@ -90,12 +22,12 @@ function calculateValveSize(rate) {
 }
 
 function assignPipeAndValveSizeValuesForXYZ(rateX, rateY) {
-  rateZ = rateX + rateY;
-  pipeSizeX = calculatePipeSize(rateX);
-  valveSizeX = calculateValveSize(rateX);
-  pipeSizeY = calculatePipeSize(rateY);
-  valveSizeY = calculateValveSize(rateY);
-  pipeSizeZ = calculatePipeSize(rateZ);
+  pipeZ.rate = rateX + rateY;
+  pipeX.pipeSize = calculatePipeSize(rateX);
+  pipeX.valveSize = calculateValveSize(rateX);
+  pipeY.pipeSize = calculatePipeSize(rateY);
+  pipeY.valveSize = calculateValveSize(rateY);
+  pipeZ.pipeSize = calculatePipeSize(pipeZ.rate);
   return;
 }
 
@@ -119,13 +51,124 @@ function loopThroughPipeOrValveArray(pipeOrValveSize, pipeOrValveArray) {
 function findMinimumPipeAndValveSizeForXYZ(rateX, rateY) {
   assignPipeAndValveSizeValuesForXYZ(rateX, rateY);
 
-  minPipeSizeX = loopThroughPipeOrValveArray(pipeSizeX, PIPE_SIZES);
-  minValveSizeX = loopThroughPipeOrValveArray(valveSizeX, VALVE_SIZES);
+  minPipeX.pipeSize = loopThroughPipeOrValveArray(pipeX.pipeSize, PIPE_SIZES);
+  minPipeX.valveSize = loopThroughPipeOrValveArray(pipeX.valveSize, VALVE_SIZES);
 
-  minPipeSizeY = loopThroughPipeOrValveArray(pipeSizeY, PIPE_SIZES);
-  minValveSizeY = loopThroughPipeOrValveArray(valveSizeY, VALVE_SIZES);
+  minPipeY.pipeSize = loopThroughPipeOrValveArray(pipeY.pipeSize, PIPE_SIZES);
+  minPipeY.valveSize = loopThroughPipeOrValveArray(pipeY.valveSize, VALVE_SIZES);
 
-  minPipeSizeZ = loopThroughPipeOrValveArray(pipeSizeZ, PIPE_SIZES);
+  minPipeZ.pipeSize = loopThroughPipeOrValveArray(pipeZ.pipeSize, PIPE_SIZES);
 }
 
 findMinimumPipeAndValveSizeForXYZ(16.45, 18.9);
+console.log(pipeX);
+console.log(minPipeX);
+
+//D3
+
+var boxCoord = [[400, 100], [400, 700], [900, 700], [900, 100], [400, 100]];
+
+var pipeXCoordBeforeArc = [[90, 300], [500, 300]];
+var pipeYCoordBeforeArc = [[90, 450], [500, 450]];
+
+var pipeXCoordAfterArc = [[696, 300], [775, 300]];
+var pipeYCoordAfterArc = [[696, 450], [775, 450]];
+
+var pipeZVerticalCoord = [[775, 450], [775, 200]];
+var pipeZHorizontalCoord = [[771, 200], [1200, 200]];
+
+var svgContainer = d3
+  .select('body')
+  .append('svg')
+  .attr('width', 1280)
+  .attr('height', 815)
+  .style('border', '1px solid black');
+
+var lineGenerator = d3.line();
+var boxPath = lineGenerator(boxCoord);
+
+var pipeXPathBeforeArc = lineGenerator(pipeXCoordBeforeArc);
+var pipeXPathAfterArc = lineGenerator(pipeXCoordAfterArc);
+
+var pipeYPathBeforeArc = lineGenerator(pipeYCoordBeforeArc);
+var pipeYPathAfterArc = lineGenerator(pipeYCoordAfterArc);
+
+var pipeZVerticalPath = lineGenerator(pipeZVerticalCoord);
+var pipeZHorizontalPath = lineGenerator(pipeZHorizontalCoord);
+
+var box = svgContainer
+  .append('path')
+  .attr('d', boxPath)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8)
+  .attr('fill', 'none');
+
+var pipeXLineBeforeArc = svgContainer
+  .append('path')
+  .attr('d', pipeXPathBeforeArc)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8)
+  .attr('fill', 'none');
+
+var pipeXLineAfterArc = svgContainer
+  .append('path')
+  .attr('d', pipeXPathAfterArc)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8)
+  .attr('fill', 'none');
+
+var pipeYLineBeforeArc = svgContainer
+  .append('path')
+  .attr('d', pipeYPathBeforeArc)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8)
+  .attr('fill', 'none');
+
+var pipeYLineAfterArc = svgContainer
+  .append('path')
+  .attr('d', pipeYPathAfterArc)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8)
+  .attr('fill', 'none');
+
+var pipeZVerticalLine = svgContainer
+  .append('path')
+  .attr('d', pipeZVerticalPath)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8)
+  .attr('fill', 'none');
+
+var pipeZHorizontalLine = svgContainer
+  .append('path')
+  .attr('d', pipeZHorizontalPath)
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8)
+  .attr('fill', 'none');
+
+var arcLineX = svgContainer
+  .append('path')
+  .attr('d', 'M 496, 300 A 50, 50, 0, 0 ,1, 700, 300')
+  .attr('fill', 'none')
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8);
+
+var arcLineY = svgContainer
+  .append('path')
+  .attr('d', 'M496, 450 A 50, 50, 0, 0, 1, 700, 450')
+  .attr('fill', 'none')
+  .attr('stroke', 'black')
+  .attr('stroke-width', 8);
+
+var circleX = svgContainer
+  .append('circle')
+  .attr('cx', 775)
+  .attr('cy', 300)
+  .attr('r', 20)
+  .attr('fill', 'black');
+
+var circleY = svgContainer
+  .append('circle')
+  .attr('cx', 775)
+  .attr('cy', 450)
+  .attr('r', 20)
+  .attr('fill', 'black');
